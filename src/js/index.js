e4c17efd450a2cd725ee31a189acd02d057994d3
Vladimir <shadrov_vladimir@mail.ru>
2021-02-01 16:08:44 +0300

    Добавлена верстка блока Footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../styles/styles.scss';
 import '../styles/header.scss';
+import '../styles/footer.scss';
 
 //Перенос фотографий в папку dist
 require.context('../images', true, /\.(png|jpg|svg|gif)$/);

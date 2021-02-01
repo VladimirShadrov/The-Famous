143ee5232df24ae2c44bc8539445ac2e0d74ed09
Vladimir <shadrov_vladimir@mail.ru>
2021-02-01 17:58:03 +0300

    Добавлен блок с основным контентом

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../styles/styles.scss';
 import '../styles/header.scss';
 import '../styles/footer.scss';
+import '../styles/pictures.scss';
 
 //Перенос фотографий в папку dist
 require.context('../images', true, /\.(png|jpg|svg|gif)$/);

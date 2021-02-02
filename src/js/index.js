da6752bd5f83b4c56a01642b4a3509f408d5bd54
Vladimir <shadrov_vladimir@mail.ru>
2021-02-02 10:10:49 +0300

    Добавлен функционал кнопки КУПИТЬ: отправка запроса, отрисовка лоадера, изменение внешнего вида кнопки после удачного запроса

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,43 @@ import '../styles/header.scss';
 import '../styles/footer.scss';
 import '../styles/pictures.scss';
 
+const buttons = Array.from(document.querySelectorAll('.pictures__item-btn'));
+
 //Перенос фотографий в папку dist
 require.context('../images', true, /\.(png|jpg|svg|gif)$/);
+
+// Создать лоадер
+function createLoader(item) {
+  item.innerHTML = '';
+  let loader = document.createElement('div');
+  loader.classList.add('pictures__buy-loader');
+  item.insertAdjacentElement('beforeend', loader);
+}
+
+// Добавить в корзину
+function addToShoppingCart(item) {
+  item.innerHTML = '';
+  item.textContent = 'В корзине';
+  item.style.transition = '0s';
+  item.classList.add('pictures__item-btn_in-shopping-cart');
+  setTimeout(() => item.style.transition = '0.3s', 100)
+}
+
+// Запрос на сервер
+async function setRequest(item) {
+  const url = 'https://jsonplaceholder.typicode.com/posts/1';
+  createLoader(item);
+  const response = await fetch(url);
+  
+  if (await response) {
+    addToShoppingCart(item);
+  }
+
+}
+
+buttons.forEach(item => {
+  item.addEventListener('click', () => {
+    setRequest(item);
+  })
+})
+
